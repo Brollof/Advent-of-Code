a5a4f6a3e6941eeb45ffcd4aba8f79c04106f99e
Brollof <thefish3r@gmail.com>
2021-10-14 09:32:08 +0200

    2015/day17 modified

--- 2015/day17.py
+++ 2015/day17.py
@@ -1,6 +1,6 @@
 import re
 from itertools import combinations
-
+from collections import defaultdict
 
 with open("inp17.txt") as file:
     data = file.read()
@@ -27,3 +27,22 @@ for i in range(1, len(cont)+1):
 
 print(f"Part 2: {total}")
 assert 18 == total
+
+
+################################################################
+# Part 1 only
+################################################################
+
+from functools import cache
+
+@cache
+def go(n, target):
+    if target == 0:
+        return 1
+
+    if n < 0 or target < 0:
+        return 0
+
+    return go(n - 1, target) + go(n - 1, target - cont[n])
+
+print(go(len(cont) - 1, 150))
