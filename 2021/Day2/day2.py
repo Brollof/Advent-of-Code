a407cc5e5cbf93d5c007a0635ecfc2cea1ffe8b2
Brollof <thefish3r@gmail.com>
2021-12-02 09:24:59 +0100

    Day2 done

--- 2021/Day2/day2.py
+++ 2021/Day2/day2.py
@@ -1,6 +1,30 @@
-
 with open("inp2.txt") as file:
     data = file.read()
 
-print(data)
 
+horizontal_pos = 0
+depth_1, depth_2 = 0, 0
+aim = 0
+
+for line in data.splitlines():
+    cmd, value = line.split()
+    value = int(value)
+    match cmd:
+        case 'forward':
+            horizontal_pos += value
+            depth_2 += aim * value
+        case 'up':
+            depth_1 -= value
+            aim -= value
+        case 'down':
+            depth_1 += value
+            aim += value
+
+
+p1 = depth_1 * horizontal_pos
+print(f"Part 1: {p1}")
+assert 1694130 == p1
+
+p2 = depth_2 * horizontal_pos
+print(f"Part 2: {p2}")
+assert 1698850445 == p2
