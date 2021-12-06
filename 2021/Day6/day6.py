3141a901224cc7d2b3a12022dcc47b4f15fc29c8
Brollof <thefish3r@gmail.com>
2021-12-06 14:32:06 +0100

    Day6 done

--- 2021/Day6/day6.py
+++ 2021/Day6/day6.py
@@ -1,6 +1,24 @@
+from collections import deque
+
+
+def shift(days):
+    for day in range(days):
+        cycles.rotate(-1)
+        cycles[6] += cycles[-1]
+        cycles[8] = cycles[-1]
+
 
 with open("inp6.txt") as file:
-    data = file.read()
+    fishes = [int(n) for n in file.read().split(",")]
 
-print(data)
+cycles = deque(fishes.count(i) for i in range(9))
 
+shift(80)
+p1 = sum(cycles)
+print(f"Part 1: {p1}")
+assert 380243 == p1
+
+shift(256 - 80)
+p2 = sum(cycles)
+print(f"Part 2: {p2}")
+assert 1708791884591 == p2
