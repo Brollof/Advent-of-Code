85f29944ea511c1d79977c7f70f621f82ba99330
Brollof <thefish3r@gmail.com>
2021-12-12 19:25:13 +0100

    Day10 done

--- 2021/Day10/day10.py
+++ 2021/Day10/day10.py
@@ -1,6 +1,34 @@
+from functools import reduce
+
 
 with open("inp10.txt") as file:
     data = file.read()
 
-print(data)
 
+brackets = {'{': '}', '[': ']', '(': ')', '<': '>'}
+syntax_scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
+autocomplete_scores = {'(': 1, '[': 2, '{': 3, '<': 4}
+
+syntax_error_score = 0
+auto = []
+
+for line in data.splitlines():
+    stack = []
+    for c in line:
+        if c in '[{(<':
+            stack.append(c)
+        elif c != brackets[stack[-1]]:
+            syntax_error_score += syntax_scores[c]
+            break
+        elif c == brackets[stack[-1]]:
+            stack.pop()
+    else:
+        auto.append(reduce(lambda v, c: v * 5 + autocomplete_scores[c], reversed(stack), 0))
+
+
+print(f"Part 1: {syntax_error_score}")
+assert syntax_error_score == 387363
+
+p2 = sorted(auto)[len(auto) // 2]
+print(f"Part 2: {p2}")
+assert p2 == 4330777059
